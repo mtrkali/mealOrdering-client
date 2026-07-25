0b6c6e1f7307fee2bdd7b55e32d600ee00abc0a1
mtrkali <tarakfeni95@gmail.com>
2026-07-24 19:37:02 -0700

    fixed navbar user null state

--- src/pages/Home/Navbar/Navbar.tsx
+++ src/pages/Home/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import { useAuth } from "@/context/AuthContext";
 import Link from "next/link";
 
 export default function Navbar() {
-    const { user, loading } = useAuth();
-    if (loading) return;
+    const { user, loading } = useAuth() || {}
+    if (loading) <div className="user loading..."></div>;
     return (
         <nav className="w-full border-b shadow-sm bg-green-400 sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-6 py-2 flex items-center justify-between">
