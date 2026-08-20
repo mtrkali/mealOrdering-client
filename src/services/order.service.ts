99fd629bd0c78c8e6fa099719cc09c87b94bbe77
mtrkali <tarakfeni95@gmail.com>
2026-08-19 20:35:29 -0700

    added getMySingleOrder function in servicefile

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -29,7 +29,18 @@ const getMyOrders = async () => {
     return response.data;
 }
 
+const getMySingleOrder = async (orderId: string) => {
+    const response = await axios.get(
+        `${API_URL}/api/v1/orders/me/${orderId}`,
+        {
+            withCredentials: true,
+        }
+    )
+    return response.data;
+}
+
 export const orderService = {
     createOrder,
     getMyOrders,
+    getMySingleOrder,
 }
