3d2e0f79d82e6df0f486f416e005957760922875
mtrkali <tarakfeni95@gmail.com>
2026-09-18 07:26:55 -0700

    added category management box is admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -205,6 +205,25 @@ export default function AdminDashboardPage() {
                         go to applications <FaArrowRight size={18} />
                     </button>
                 </div>
+
+                {/* category mangement */}
+                <div data-aos="zoom-out" data-aos-delay="2200" className="border rounded-lg p-6 shadow-sm ">
+                    <p className="text-gray-500">
+                        Total provider application
+                    </p>
+
+                    <h2 className="mt-2 text-3xl font-bold">
+                        {stats.totalApplicantProvider}
+                    </h2>
+
+                    <button
+                        type="button"
+                        onClick={() => router.push("/admin/category")}
+                        className="mt-4 hover:scale-105 transition flex gap-1 items-center border px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700"
+                    >
+                        go to category <FaArrowRight size={18} />
+                    </button>
+                </div>
             </div>
         </main>
     );
